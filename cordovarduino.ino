3475a79aa24818271f3abe4fb593ae8ae8428326
Андрей Кумыков <aakumykov@yandex.ru>
2016-11-25 16:40:31 +0700

    5: наконец, всё работает

--- cordovarduino.ino
+++ cordovarduino.ino
@@ -1,87 +1,56 @@
-#include <Wire.h>
 #include <LiquidCrystal_PCF8574.h>
 LiquidCrystal_PCF8574 lcd(0x27);
 
-unsigned long readInterval = 1;
-unsigned long displayInterval = 400;
-unsigned long lastRead = 0;
-unsigned long lastDisplay = 0;
+byte piece;
+String tempString = "";
 String inputData = "";
-char lineEnd = ';';
-String bytesPrefix = "inBytes:";
-String dataPrefix = "inData:";
+unsigned long displayInterval = 200;
+unsigned long lastDisplay = 0;
+String bytesPrefix = "bytes:";
+String dataPrefix = "data:";
 int bytesPrefixLength = bytesPrefix.length();
 int dataPrefixLength = dataPrefix.length();
 
-
-//String readInputData(byte len){
-//String readInputData(){
-//  int incomingBytes = Serial.available();
-//  //Serial.println("input bytes: "+String(incomingBytes));
-//  
-//  String data = "";
-//  
-//  if (incomingBytes > 0) {
-//    displayBytes(incomingBytes);
-//    
-//    for (int i=0; i<incomingBytes; i++) {
-//      data += String( char(Serial.read()) );
-//    }
-//    return data;
-//  }
-//  
-//  return "";
-//}
-
-
 void displayBytes(int b){
-  String msg = String(b);
-  lcd.setCursor(bytesPrefixLength+1, 0);
-  lcd.print(msg);
+  lcd.setCursor(bytesPrefixLength, 0);
+  lcd.print(String(b));
 }
 
 void displayData(String d){
-  int len = dataPrefix.length();
-  lcd.setCursor(dataPrefixLength+1, 1);
+  lcd.setCursor(dataPrefixLength, 1);
   lcd.print(d);
 }
 
-
 void setup() {
-    Serial.begin(9600);
-    
-    lcd.begin(16,2);
-    lcd.setBacklight(255);
-    lcd.clear();
+  Serial.begin(9600);
+  while(!Serial);
 
-    lcd.setCursor(0,0);
-    lcd.print("*screen coords*");
-    lcd.setCursor(0,1);
-    lcd.print("R:"+String(readInterval)+", D:"+String(displayInterval));
-
-    delay(1000);
-    lcd.clear();
-
-//    lcd.setCursor(0,0); lcd.print(bytesPrefix);
-//    lcd.setCursor(0,1); lcd.print(dataPrefix);
+  lcd.begin(16,2);
+  lcd.setBacklight(1);
+  lcd.home(); lcd.clear();
+  lcd.setCursor(0,0); lcd.print(bytesPrefix);
+  lcd.setCursor(0,1); lcd.print(dataPrefix);
 }
- 
-void loop() {
-  // чтение
-  if (Serial.available() > 0){
-    inputData = "";
-    String inputData = Serial.readStringUntil(lineEnd);
-  }
 
-  // отображение
-  unsigned long currentDisplay = millis();
-  if ((currentDisplay - lastDisplay) > displayInterval) {
-//    if (inputData != "") {
-//      displayData(inputData);
-//    }
-    lcd.home();
-    lcd.print(inputData);
-    lastDisplay = currentDisplay;
+void loop() {
+  if (Serial.available() > 0) {
+      piece = Serial.read();
+      if (piece != ';') {
+        tempString += String(char(piece));
+      } else {
+        inputData = tempString;
+        tempString = "";
+      }
+  }
+  
+  
+  if ((millis() - lastDisplay) > displayInterval) {
+    if (inputData != "") {
+      Serial.print(" RECIEVED: "+inputData);
+      displayBytes(inputData.length());
+      displayData(inputData);
+    }
+    lastDisplay = millis();
   }
 }
 
